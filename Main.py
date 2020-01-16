7b114728512311890f53f74d6cccf535fbea6674
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-01-16 07:50:40 +0600

    New knowledge and questions added

--- Main.py
+++ Main.py
@@ -13,14 +13,14 @@ import json
 
 
 def text_to_speech(txt):
-		language = "en"
-		obj = gTTS(text = txt, lang = language)
-		obj.save("output.mp3")
-		pygame.mixer.init()
-		pygame.mixer.music.load("output.mp3")
-		pygame.mixer.music.play()
-		while pygame.mixer.music.get_busy() == True:
-			continue
+	language = "en"
+	obj = gTTS(text = txt, lang = language)
+	obj.save("output.mp3")
+	pygame.mixer.init()
+	pygame.mixer.music.load("output.mp3")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+		continue
 
 
 
@@ -53,12 +53,12 @@ def ask_question(query_string, question):
 
 
 
-def say_answers(prefix, question, answers):
+def say_answers(prefix, suffix, question, answers):
 	for ans in answers:
 		ans = make_json(str(ans))
 		obj = json.loads(str(ans))
 		print(obj[question])
-		text = prefix + " " + obj[question]
+		text = prefix + " " + obj[question] + " " + suffix
 		text_to_speech(text)
 
 
@@ -80,7 +80,6 @@ def listen_question():
 
 # Q/A
 asked_question = str(listen_question()).lower()
-print("history of jahangirnagar university" in asked_question)
 print(asked_question)
 
 if "name of the university" in asked_question :
@@ -88,16 +87,16 @@ if "name of the university" in asked_question :
 	question = "UniversityName"
 	query = "name(" + question + ")."
 	answers = ask_question(query, question)
-	say_answers("The name of the university ", question, answers)
+	say_answers("The name of the university ", "", question, answers)
 
 elif "introduction" in asked_question or \
-	"something about ju" in asked_question or \
-	"something about jahangirnagar university" in asked_question:
+	"about ju" in asked_question or \
+	"about jahangirnagar university" in asked_question:
 	# Q: what is jahangirnagar university?
 	question = "Introduction"
 	query = "introduction('jahangirnagar university', " + question + ")."
 	answers = ask_question(query, question)
-	say_answers("", question, answers)
+	say_answers("", "", question, answers)
 
 elif "history of ju" in asked_question or \
 		"history of jahangirnagar university" in asked_question:
@@ -105,7 +104,7 @@ elif "history of ju" in asked_question or \
 	question = "History"
 	query = "history('jahangirnagar university', " + question + ")."
 	answers = ask_question(query, question)
-	say_answers("Brief history: ", question, answers)
+	say_answers("Brief history: ", "", question, answers)
 
 elif "location of jahangirnagar university" in asked_question or \
 		"situated" in asked_question:
@@ -113,14 +112,14 @@ elif "location of jahangirnagar university" in asked_question or \
 	question = "Loction"
 	query = "location('jahangirnagar university', " + question + ")."
 	answers = ask_question(query, question)
-	say_answers("", question, answers)
+	say_answers("", "", question, answers)
 
 elif "area of jahangirnagar university" in asked_question:
 	# Q: where is jahangirnagar university?
 	question = "Area"
 	query = "area('jahangirnagar university', " + question + ")."
 	answers = ask_question(query, question)
-	say_answers("total area of jahangirnagar university is about ", question, answers)
+	say_answers("total area of jahangirnagar university is about ", "", question, answers)
 
 elif ("current" in asked_question or "present" in asked_question or "now" in asked_question) \
 		and "vice chancellor" in asked_question:
@@ -128,5 +127,52 @@ elif ("current" in asked_question or "present" in asked_question or "now" in ask
 	question = "Vice_chancellor"
 	query = "vice_chancellor('jahangirnagar university', " + question + ")."
 	answers = ask_question(query, question)
-	say_answers("The current vice chancellor of jahangirnagar university is ", question, answers)
+	say_answers("The current vice chancellor of jahangirnagar university is ", "", question, answers)
 
+elif "number of faculties" in asked_question or \
+		"how many faculties" in asked_question:
+	# Q how many faculties are in jahangirnagr university
+	question = "Number_of_faculties"
+	query = "number_of_faculties('jahangirnagar university', " + question + ")."
+	answers = ask_question(query, question)
+	say_answers("There are ", "faculties in jahangirnagar university", question, answers)
+
+elif "number of departments" in asked_question or \
+		"how many departments" in asked_question:
+	# Q how many departments are in jahangirnagr university
+	question = "Number_of_departments"
+	query = "number_of_departments('jahangirnagar university', " + question + ")."
+	answers = ask_question(query, question)
+	say_answers("There are ", "departments in jahangirnagar university", question, answers)
+
+elif "number of institutes" in asked_question or \
+		"how many institutes" in asked_question:
+	# Q how many institutes are in jahangirnagr university
+	question = "Number_of_institutes"
+	query = "number_of_institutes('jahangirnagar university', " + question + ")."
+	answers = ask_question(query, question)
+	say_answers("There are ", "institutes in jahangirnagar university", question, answers)
+
+elif "name of the faculties" in asked_question or \
+		"what are the faculties" in asked_question:
+	#Q what are the faculties in jahangirnagar university
+	question = "Facultiy"
+	query = "faculties('jahangirnagar university', " + question + ")."
+	answers = ask_question(query, question)
+	text_to_speech("there are 6 faculties are in jahangirnagar university, they are ")
+	say_answers("", "", question, answers)
+
+elif "name of the departments" in asked_question or \
+		"what are the departments" in asked_question:
+	#Q what are the departments in jahangirnagar university
+	question = "Departments"
+	query = "departments('jahangirnagar university', " + question + ")."
+	answers = ask_question(query, question)
+	say_answers("there are 34 departments in jahangirnagar university, they are ", "", question, answers)
+
+elif ("names of the departments" in asked_question or \
+		"what are the departments" in asked_question) and \
+		"in faculty of" in asked_question:
+	# Q what are the names departments in faculty of X?
+	question = "Departments"
+	query = ""
--- knowledge.pl
+++ knowledge.pl
@@ -1,12 +1,66 @@
 name('jahangirnagar university').
-breif_description('Jahangirnagar University is a public university in Bangladesh, based in Savar Upazila, Dhaka. It is one of the top and only fully residential universities in Bangladesh.').
-location('jahangirnagar university stands on the west side of the Asian Highway, popularly known as the Dhaka-Aricha Road, and is 32 kilometres away from the capital').
-established('jahangirnagar university', 1970).
+breif_description('Jahangirnagar University is a public university in Bangladesh, \c
+	based in Savar Upazila, Dhaka. It is one of the top and only fully residential \c
+	universities in Bangladesh. There are 34 departments, 4 institutes under six faculties.').
+location('jahangirnagar university stands on the west side of the Asian Highway, \c
+	popularly known as the Dhaka-Aricha Road, and is 32 kilometres away from the capital').
+established('jahangirnagar university', '1970').
 first_vice_chancellor('Mafizuddin Ahmed (PhD in chemistry, Penn State)').
 current_vice_chancellor('Professor Doctor Farzan Islam').
-history('The university was established in 1970 by the Jahangirnagar Muslim University Ordinance of the government of Pakistan. Its first vice-chancellor, Mafizuddin Ahmed (PhD in chemistry, Penn State) took up office on 24 September 1970. The first group of students, a total of 150, were enrolled in four departments: Economics, Geography, Mathematics, and Statistics. Its formal inauguration was delayed until 12 January 1971, when the university was launched by Rear Admiral S. M. Ahsan, the chancellor.').
+history('The university was established in 1970 by the Jahangirnagar Muslim University \c
+	Ordinance of the government of Pakistan. Its first vice-chancellor, Mafizuddin Ahmed \c
+	(PhD in chemistry, Penn State) took up office on 24 September 1970. The first group of \c
+	students, a total of 150, were enrolled in four departments: Economics, Geography, \c
+	Mathematics, and Statistics. Its formal inauguration was delayed until 12 January 1971, \c
+	when the university was launched by Rear Admiral S. M. Ahsan, the chancellor.').
 area('697.56 acres which is 2.8 square kilometres').
+number_of_faculties('6').
+number_of_departments('34').
+number_of_institutes('4').
+faculty('faculty of mathematical and physical science').
+faculty('faculty of biological science').
+faculty('faculty of social science').
+faculty('faculty of arts and humanities').
+faculty('faculty of business studies').
+faculty('faculty of law').
 
+departments('department of computer science and engineering, \c
+	department of mathematics, \c
+	department of physics, \c
+	department of environmental science, \c
+	department of chemistry, \c
+	department of statistics, \c
+	department of geological science, \c
+	department of botany, \c
+	department of zoology, \c
+	department of biochemistry and molecular biology, \c
+	department of microbiology, \c
+	department of pharmacy, \c
+	department of public health and informatics, \c
+	department of biotechnology and genetic engineering, \c
+	department of anthropology, \c
+	department of economics, \c
+	department of government and politics,
+	department of geography and environment, \c
+	department of public administration, \c
+	department of urban and regional planning, \c
+	department of archaeology, \c
+	department of bangla, \c
+	department of drama and dramatics, \c
+	department of english, \c
+	department of fine arts, \c
+	department of history, \c
+	department of international relations, \c
+	department of journalism and media studies, \c
+	department of philosophy, \c
+	department of accounting and information systems, \c
+	department of finance and banking, \c
+	department of marketing, \c
+	department of management studies, \c
+	department of accounting and information systems, \c
+	department of finance and banking, \c
+	department of marketing, \c
+	department of management studies').
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -16,3 +70,10 @@ location(X, Y):- name(X), location(Y).
 area(X, Y):- name(X), area(Y).
 first_vice_chancellor(X, Y):- name(X), first_vice_chancellor(Y).
 vice_chancellor(X, Y):- name(X), current_vice_chancellor(Y).
+number_of_faculties(X, Y):- name(X), number_of_faculties(Y).
+number_of_departments(X, Y):- name(X), number_of_departments(Y).
+number_of_institutes(X, Y):- name(X), number_of_institutes(Y).
+
+faculties(X, Y):- name(X), faculty(Y).
+departments(X, Y):- name(X), departments(Y).
+departments_in_faculty(X, Y):- faculty(X), 
