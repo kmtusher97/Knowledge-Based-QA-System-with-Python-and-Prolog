94d010ca42cb1e9f06abcabbebe772d5dfc1c934
kmtusher97 <kmtusher97@gmail.com>
2020-01-16 09:57:04 +0600

    Q/A on faculties and departments

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # prolog
 from pyswip import Prolog
 
-# text to speech 
+# text to speech
 from gtts import gTTS
 import pygame
 
@@ -9,170 +9,211 @@ import pygame
 import speech_recognition as sr
 
 import json
-
+import time
 
 
 def text_to_speech(txt):
-	language = "en"
-	obj = gTTS(text = txt, lang = language)
-	obj.save("output.mp3")
-	pygame.mixer.init()
-	pygame.mixer.music.load("output.mp3")
-	pygame.mixer.music.play()
-	while pygame.mixer.music.get_busy() == True:
-		continue
-
+    language = "en"
+    obj = gTTS(text=txt, lang=language)
+    obj.save("output.mp3")
+    pygame.mixer.init()
+    pygame.mixer.music.load("output.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+    pygame.mixer.music.pause()
 
 
 def prolog_query(query_string):
-	prolog = Prolog()
-	prolog.consult("knowledge.pl")
-	results = []
-	for res in prolog.query(query_string):
-		results.append(res)
-
-	return results
+    prolog = Prolog()
+    prolog.consult("knowledge.pl")
+    results = []
+    for res in prolog.query(query_string):
+        results.append(res)
+    return results
 
 
 def make_json(data):
-	json_str = ""
-	for c in data:
-		if c == "'":
-			json_str += "\""
-			continue
+    json_str = ""
+    for c in data:
+        if c == "'":
+            json_str += "\""
+            continue
 
-		json_str += c
+        json_str += c
 
-	return json_str
-
-
-
-def ask_question(query_string, question):
-	answers = prolog_query(query_string)
-	return answers
+    return json_str
 
 
+def ask_question(query_string):
+    ret_answers = prolog_query(query_string)
+    return ret_answers
 
-def say_answers(prefix, suffix, question, answers):
-	for ans in answers:
-		ans = make_json(str(ans))
-		obj = json.loads(str(ans))
-		print(obj[question])
-		text = prefix + " " + obj[question] + " " + suffix
-		text_to_speech(text)
 
+def say_answers(prefix, suffix, question_i, answers_i):
+    for ansi in answers_i:
+        ansi = make_json(str(ansi))
+        obj = json.loads(str(ansi))
+        print(obj[question_i])
+        text = prefix + " " + obj[question_i] + " " + suffix
+        text_to_speech(text)
 
 
 def listen_question():
-	r = sr.Recognizer()
+    r = sr.Recognizer()
 
-	with sr.Microphone() as source:
-		print("ask question about jahangirnagar university")
-		audio = r.listen(source)
-		print("ok.. wait for the answer...")
+    with sr.Microphone() as source:
+        print("ask question about jahangirnagar university")
+        audio = r.listen(source)
+        print("ok.. wait for the answer...")
 
-	try:
-		return r.recognize_google(audio)
-	except:
-		return "-----------------"
+    try:
+        return r.recognize_google(audio)
+    except:
+        return "-----------------"
 
 
+# text_to_speech("Hi, I'm here to tell you about jahangirnagar university. \
+#                what do you want to know about jahangirnagar university?")
+flg = True
+while flg:
+    # Q/A
+    asked_question = str(listen_question()).lower()
+    print(asked_question)
 
-# Q/A
-asked_question = str(listen_question()).lower()
-print(asked_question)
+    if "name of the university" in asked_question or \
+            "university name" in asked_question:
+        # Q: what is the name of the university?
+        question = "UniversityName"
+        query = "name(" + question + ")."
+        answers = ask_question(query)
+        say_answers("The name of the university ", "", question, answers)
 
-if "name of the university" in asked_question :
-	# Q: what is the name of the university?
-	question = "UniversityName"
-	query = "name(" + question + ")."
-	answers = ask_question(query, question)
-	say_answers("The name of the university ", "", question, answers)
+    elif "introduction" in asked_question or \
+            "about ju" in asked_question or \
+            "about jahangirnagar university" in asked_question:
+        # Q: what is jahangirnagar university?
+        question = "Introduction"
+        query = "introduction('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("", "", question, answers)
 
-elif "introduction" in asked_question or \
-	"about ju" in asked_question or \
-	"about jahangirnagar university" in asked_question:
-	# Q: what is jahangirnagar university?
-	question = "Introduction"
-	query = "introduction('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("", "", question, answers)
+    elif "history of ju" in asked_question or \
+            "history of jahangirnagar university" in asked_question:
+        # Q: history of jahangirnagar university.
+        question = "History"
+        query = "history('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("Brief history: ", "", question, answers)
 
-elif "history of ju" in asked_question or \
-		"history of jahangirnagar university" in asked_question:
-	# Q: history of jahangirnagar university.
-	question = "History"
-	query = "history('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("Brief history: ", "", question, answers)
+    elif "location of jahangirnagar university" in asked_question or \
+            "situated" in asked_question:
+        # Q: where is jahangirnagar university?
+        question = "Loction"
+        query = "location('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("", "", question, answers)
 
-elif "location of jahangirnagar university" in asked_question or \
-		"situated" in asked_question:
-	# Q: where is jahangirnagar university?
-	question = "Loction"
-	query = "location('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("", "", question, answers)
+    elif "area of jahangirnagar university" in asked_question:
+        # Q: where is jahangirnagar university?
+        question = "Area"
+        query = "area('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("total area of jahangirnagar university is about ", "", question, answers)
 
-elif "area of jahangirnagar university" in asked_question:
-	# Q: where is jahangirnagar university?
-	question = "Area"
-	query = "area('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("total area of jahangirnagar university is about ", "", question, answers)
+    elif ("current" in asked_question or "present" in asked_question or "now" in asked_question) \
+            and ("vice chancellor" in asked_question or \
+                 "vc" in asked_question):
+        # Q: who is the current vice_chancellor of jahangirnagar university?
+        question = "Vice_chancellor"
+        query = "vice_chancellor('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("The current vice chancellor of jahangirnagar university is ", \
+                    "", question, answers)
 
-elif ("current" in asked_question or "present" in asked_question or "now" in asked_question) \
-		and "vice chancellor" in asked_question:
-	#Q: who is the current vice_chancellor of jahangirnagar university?
-	question = "Vice_chancellor"
-	query = "vice_chancellor('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("The current vice chancellor of jahangirnagar university is ", "", question, answers)
+    elif "number of faculties" in asked_question or \
+            "how many faculties" in asked_question:
+        # Q how many faculties are in jahangirnagr university
+        question = "Number_of_faculties"
+        query = "number_of_faculties('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("There are ", "faculties in jahangirnagar university", question, answers)
 
-elif "number of faculties" in asked_question or \
-		"how many faculties" in asked_question:
-	# Q how many faculties are in jahangirnagr university
-	question = "Number_of_faculties"
-	query = "number_of_faculties('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("There are ", "faculties in jahangirnagar university", question, answers)
+    elif "number of departments" in asked_question or \
+            "how many departments" in asked_question:
+        # Q how many departments are in jahangirnagr university
+        question = "Number_of_departments"
+        query = "number_of_departments('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("There are ", "departments in jahangirnagar university", question, answers)
 
-elif "number of departments" in asked_question or \
-		"how many departments" in asked_question:
-	# Q how many departments are in jahangirnagr university
-	question = "Number_of_departments"
-	query = "number_of_departments('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("There are ", "departments in jahangirnagar university", question, answers)
+    elif "number of institutes" in asked_question or \
+            "how many institutes" in asked_question:
+        # Q how many institutes are in jahangirnagr university
+        question = "Number_of_institutes"
+        query = "number_of_institutes('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("There are ", "institutes in jahangirnagar university", question, answers)
 
-elif "number of institutes" in asked_question or \
-		"how many institutes" in asked_question:
-	# Q how many institutes are in jahangirnagr university
-	question = "Number_of_institutes"
-	query = "number_of_institutes('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("There are ", "institutes in jahangirnagar university", question, answers)
+    elif "names of the faculties" in asked_question or \
+            "what are the faculties" in asked_question:
+        # Q what are the faculties in jahangirnagar university
+        question = "Facultiy"
+        query = "faculties('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("there are 6 faculties are in jahangirnagar university, they are, ", \
+                    "", question, answers)
 
-elif "name of the faculties" in asked_question or \
-		"what are the faculties" in asked_question:
-	#Q what are the faculties in jahangirnagar university
-	question = "Facultiy"
-	query = "faculties('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	text_to_speech("there are 6 faculties are in jahangirnagar university, they are ")
-	say_answers("", "", question, answers)
+    elif ("names of the departments" in asked_question or \
+          "what are the departments" in asked_question) and \
+            "under the faculty of" in asked_question:
+        # Q what are the names departments in faculty of X?
+        faculties = ["faculty of mathematical and physical science", \
+                     "faculty of biological science", \
+                     "faculty of social science", \
+                     "faculty of arts and humanities", \
+                     "faculty of business studies", \
+                     "faculty of law"]
+        id = -1
+        for i in range(6):
+            if faculties[i] in asked_question:
+                id = i
+                break
+        if id != -1:
+            print(faculties[id])
+            question = "Departments"
+            query = "departments_under_faculty('jahangirnagar university', '" + \
+                    faculties[id] + "'," + question + ")."
+            answers = ask_question(query)
+            text_to_speech("the departments under " + faculties[id] + " are, ")
+            say_answers("", "", question, answers)
 
-elif "name of the departments" in asked_question or \
-		"what are the departments" in asked_question:
-	#Q what are the departments in jahangirnagar university
-	question = "Departments"
-	query = "departments('jahangirnagar university', " + question + ")."
-	answers = ask_question(query, question)
-	say_answers("there are 34 departments in jahangirnagar university, they are ", "", question, answers)
+        else:
+            text_to_speech("sorry, there is no such faculty.")
 
-elif ("names of the departments" in asked_question or \
-		"what are the departments" in asked_question) and \
-		"in faculty of" in asked_question:
-	# Q what are the names departments in faculty of X?
-	question = "Departments"
-	query = ""
+    elif "names of the departments" in asked_question or \
+            "what are the departments" in asked_question:
+        # Q what are the departments in jahangirnagar university
+        question = "Departments"
+        query = "departments('jahangirnagar university', " + question + ")."
+        answers = ask_question(query)
+        say_answers("there are 34 departments in jahangirnagar university, they are, ", \
+                    "", question, answers)
+
+    elif "stop" in asked_question or "exit" in asked_question:
+        text_to_speech("thank you, hope you have enjoyed the session")
+        break
+
+    else:
+        if asked_question != "-----------------":
+            text_to_speech("Sorry, this is out of my knowledge. whould you like to continue?")
+            confirmation = str(listen_question()).lower()
+            print(confirmation)
+            if "no" in confirmation or "nope" in confirmation or "stop" in confirmation:
+                text_to_speech("thank you, hope you have enjoyed the session")
+                break
+            else:
+                time.sleep(5)
+                continue
+
+        time.sleep(5)
--- knowledge.pl
+++ knowledge.pl
@@ -24,6 +24,13 @@ faculty('faculty of arts and humanities').
 faculty('faculty of business studies').
 faculty('faculty of law').
 
+faculties('faculty of mathematical and physical science, \c
+	faculty of biological science, \c
+	faculty of social science, \c
+	faculty of arts and humanities, \c
+	faculty of business studies, \c
+	faculty of law').
+
 departments('department of computer science and engineering, \c
 	department of mathematics, \c
 	department of physics, \c
@@ -62,6 +69,48 @@ departments('department of computer science and engineering, \c
 	department of marketing, \c
 	department of management studies').
 
+departments_under_faculty('faculty of mathematical and physical science','department of computer science and engineering').
+departments_under_faculty('faculty of mathematical and physical science','department of mathematics').
+departments_under_faculty('faculty of mathematical and physical science','department of physics').
+departments_under_faculty('faculty of mathematical and physical science','department of environmental science').
+departments_under_faculty('faculty of mathematical and physical science','department of chemistry').
+departments_under_faculty('faculty of mathematical and physical science','department of statistics').
+departments_under_faculty('faculty of mathematical and physical science','department of geological science').
+
+departments_under_faculty('faculty of biological science','department of botany').
+departments_under_faculty('faculty of biological science','department of zoology').
+departments_under_faculty('faculty of biological science','department of biochemistry and molecular biology').
+departments_under_faculty('faculty of biological science','department of microbiology').
+departments_under_faculty('faculty of biological science','department of pharmacy').
+departments_under_faculty('faculty of biological science','department of public health and informatics').
+departments_under_faculty('faculty of biological science','department of biotechnology and genetic engineering').
+
+departments_under_faculty('faculty of social science','department of anthropology').
+departments_under_faculty('faculty of social science','department of economics').
+departments_under_faculty('faculty of social science','department of government and politics').
+departments_under_faculty('faculty of social science','department of geography and environment').
+departments_under_faculty('faculty of social science','department of public administration').
+departments_under_faculty('faculty of social science','department of urban and regional planning').
+
+departments_under_faculty('faculty of arts and humanities','department of archaeology').
+departments_under_faculty('faculty of arts and humanities','department of bangla').
+departments_under_faculty('faculty of arts and humanities','department of drama and dramatics').
+departments_under_faculty('faculty of arts and humanities','department of english').
+departments_under_faculty('faculty of arts and humanities','department of fine arts').
+departments_under_faculty('faculty of arts and humanities','department of history').
+departments_under_faculty('faculty of arts and humanities','department of international relations').
+departments_under_faculty('faculty of arts and humanities','department of journalism and media studies').
+departments_under_faculty('faculty of arts and humanities','department of philosophy').
+
+departments_under_faculty('faculty of business studies','department of accounting and information systems').
+departments_under_faculty('faculty of business studies','department of finance and banking').
+departments_under_faculty('faculty of business studies','department of marketing').
+departments_under_faculty('faculty of business studies','department of management studies').
+
+departments_under_faculty('faculty of law','department of law and justice').
+
+department('department of computer science and engineering').
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 introduction(X, Y):- name(X), breif_description(Y).
@@ -74,6 +123,6 @@ number_of_faculties(X, Y):- name(X), number_of_faculties(Y).
 number_of_departments(X, Y):- name(X), number_of_departments(Y).
 number_of_institutes(X, Y):- name(X), number_of_institutes(Y).
 
-faculties(X, Y):- name(X), faculty(Y).
+faculties(X, Y):- name(X), faculties(Y).
 departments(X, Y):- name(X), departments(Y).
-departments_in_faculty(X, Y):- faculty(X), 
+departments_under_faculty(X, Y, Z):- name(X), faculty(Y), departments_under_faculty(Y, Z).
